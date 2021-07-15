7edf75a65a66e1a6f210c4421538e689c7c38237
Matthias Nemeth <manedev79@gmail.com>
2021-07-15 21:55:56 +0200

    feat: read remote zen

--- src/features/ZenFeature.js
+++ src/features/ZenFeature.js
@@ -1,6 +1,16 @@
-const axios = require('axios')
+const axios = require('axios').default;
 
 const condition = /Zen|Weisheit|Schlaues/
+const ZEN_URL = 'https://api.github.com/zen'
+
+async function getZen(ctx) {
+    try {
+      const response = await axios.get(ZEN_URL);
+      ctx.replyWithMarkdown(`Think about that:  \n_${response.data}_`)
+    } catch (error) {
+      ctx.reply('No zen at the moment 🤤')
+    }
+  }
 
 exports.condition = condition
-exports.addTo = (bot) => bot.hears(condition, (ctx) => ctx.reply(`🤣🤣🤣 ${ctx.from.first_name}`))
+exports.addTo = (bot) => bot.hears(condition, (ctx) => getZen(ctx))
