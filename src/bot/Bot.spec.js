322e1c56a75af93bd1f18ed9d25b4a3909b1a9a7
Matthias Nemeth <manedev79@gmail.com>
2021-07-15 21:00:07 +0200

    feat: Integration test for bot configuration

---
+++ src/bot/Bot.spec.js
@@ -0,0 +1,33 @@
+const Bot = require('./Bot')
+const GreetingFeature = require('../features/GreetingFeature')
+const LaughingFeature = require('../features/LaughingFeature')
+const { Telegraf } = require('telegraf')
+jest.mock('telegraf')
+
+const BOT_TOKEN = '12345_bot_token'
+const URL = 'http://localhost/whatever'
+const PORT = '1234'
+
+var bot
+
+beforeEach(() => {
+    Telegraf.mockClear();
+    bot = Bot.createBot(BOT_TOKEN, URL, PORT);
+});
+
+test('Bot is constructed', () => {
+    expect(Telegraf).toHaveBeenCalledTimes(1);
+});
+
+test('Help command is configured', () => {
+    const botMock = Telegraf.mock.instances[0]
+
+    expect(botMock.help).toHaveBeenCalledTimes(1);
+})
+
+test('Features are configured', () => {
+    const botMock = Telegraf.mock.instances[0]
+    
+    expect(botMock.hears.mock.calls[0][0]).toBe(GreetingFeature.condition);
+    expect(botMock.hears.mock.calls[1][0]).toBe(LaughingFeature.condition);
+})
