79b17bc918c5029deac60c664c6a0da4d1ae0daa
Matthias Nemeth <manedev79@gmail.com>
2021-07-15 22:45:52 +0200

    Add help for Zen and others

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -1,1 +1,11 @@
-exports.addTo = (bot) => bot.help((ctx) => ctx.reply('Say hi or laugh 😉'));
+exports.addTo = (bot) => bot.help(
+    (ctx) => ctx.replyWithMarkdown(
+`
+*Commands*  
+/help - Du bist hier
+/version - Zeigt die aktuelle Bot Version an
+  
+*Features*  
+- Sag _hi_ oder lache  😉
+- Willst Du was Schlaues, Weisheiten oder Zen hören?
+`));
