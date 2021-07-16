c8685b6c43ab6d9dd306dac4395bccb0c913251b
Matthias Nemeth <manedev79@gmail.com>
2021-07-16 09:48:13 +0200

    Refactor help command and add test

--- src/commands/HelpCommand.js
+++ src/commands/HelpCommand.js
@@ -1,11 +1,11 @@
-exports.addTo = (bot) => bot.help(
-    (ctx) => ctx.replyWithMarkdown(
-`
-*Commands*  
-/help - Du bist hier
-/version - Zeigt die aktuelle Bot Version an
-  
-*Features*  
-- Sag _hi_ oder lache  😉
-- Willst Du was Schlaues, Weisheiten oder Zen hören?
-`));
+exports.helpCommand = (ctx) => ctx.replyWithMarkdown(
+    `
+    *Commands*  
+    /help - Du bist hier
+    /version - Zeigt die aktuelle Bot Version an
+      
+    *Features*  
+    - Sag _hi_ oder lache  😉
+    - Willst Du was Schlaues, Weisheiten oder Zen hören?
+    `)
+exports.addTo = (bot) => bot.help(this.helpCommand);
